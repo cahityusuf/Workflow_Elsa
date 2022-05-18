294a53f804fd9589c53e5504392fd5b31ac55da9
Cahit Yusuf KAFADAR <cahityusuf@gmail.com>
2022-05-18 21:38:24 +0300

    first commit

---
+++ ElsaQuickstarts.Server.ApiEndpoints/Program.cs
@@ -0,0 +1,17 @@
+namespace ElsaQuickstarts.Server.ApiEndpoints
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
